15eb723598322ba5c153e279396c8024d521660a
Vedansh Jain <vedanshjain5636@gmail.com>
2025-12-08 20:10:53 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,30 +1,10 @@
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h>
 #include "utils.h"
 
-int strncasecmp(const char *s1, const char *s2, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        unsigned char c1 = tolower((unsigned char)s1[i]);
-        unsigned char c2 = tolower((unsigned char)s2[i]);
-        if (c1 != c2 || c1 == '\0' || c2 == '\0')
-            return c1 - c2;
-    }
-    return 0;
-}
 
-char* my_strcasestr(const char* haystack, const char* needle) {
-    if (!*needle) return (char*)haystack;
-
-    size_t nlen = strlen(needle);
-
-    for (; *haystack; haystack++) {
-        if (tolower((unsigned char)*haystack) ==
-            tolower((unsigned char)*needle)) {
-            if (strncasecmp(haystack, needle, nlen) == 0)
-                return (char*)haystack;
-        }
-    }
-    return NULL;
+void clear_input_buffer() {
+int c;
+while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
 void strip_newline(char *s) {
