dd19400dfeab8446e6a3863f5e3e90625755aa95
Vedansh Jain <vedanshjain5636@gmail.com>
2025-12-08 20:08:11 +0530

    Update utlis.h

--- include/utlis.h
+++ include/utlis.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-char* my_strcasestr(const char* haystack, const char* needle);
-void strip_newline(char *s);
-int strncasecmp(const char *s1, const char *s2, size_t n);
+
+void clear_input_buffer();
+
 
 #endif
