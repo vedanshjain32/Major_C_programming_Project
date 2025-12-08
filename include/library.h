4ef9662368614081b4fdf21c81221f32361c4a94
Vedansh Jain <vedanshjain5636@gmail.com>
2025-12-08 20:07:46 +0530

    Update library.h

--- include/library.h
+++ include/library.h
@@ -1,25 +1,30 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+
 #define MAX_BOOKS 1000
 #define TITLE_LEN 100
 #define AUTHOR_LEN 100
 #define DATAFILE "library.dat"
 
+
 typedef struct {
-    int id;
-    char title[TITLE_LEN];
-    char author[AUTHOR_LEN];
-    int total;
-    int available;
+int id;
+char title[TITLE_LEN];
+char author[AUTHOR_LEN];
+int total;
+int available;
 } Book;
+
 
 extern Book books[MAX_BOOKS];
 extern int book_count;
 
+
 void load_data();
 void save_data();
-
+int next_id();
+int find_index_by_id(int id);
 void add_book();
 void list_books();
 void search_books();
@@ -27,8 +32,5 @@ void issue_book();
 void return_book();
 void remove_book();
 
-int find_index_by_id(int id);
-int next_id();
-void menu();
 
 #endif
